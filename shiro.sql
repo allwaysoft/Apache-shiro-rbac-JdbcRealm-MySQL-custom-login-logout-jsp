5e59e47d7111e718496d10018ee45fee5102dce8
allwaysoft <49237778+allwaysoft@users.noreply.github.com>
2022-09-26 11:54:59 +0800

    Add files via upload

--- shiro.sql
+++ shiro.sql
@@ -24,15 +24,13 @@ CREATE TABLE IF NOT EXISTS `permissions` (
   PRIMARY KEY (`name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
--- 正在导出表  shiro.permissions 的数据：~2 rows (大约)
+-- 正在导出表  shiro.permissions 的数据：~20 rows (大约)
 /*!40000 ALTER TABLE `permissions` DISABLE KEYS */;
 INSERT IGNORE INTO `permissions` (`name`, `description`) VALUES
-	('DELETE', 'delete'),
 	('permission-create', 'permission-create'),
 	('permission-delete', 'permission-delete'),
 	('permission-index', 'permission-index'),
 	('permission-update', 'permission-update'),
-	('READ', 'read'),
 	('role-create', 'role-create'),
 	('role-delete', 'role-delete'),
 	('role-index', 'role-index'),
@@ -48,8 +46,7 @@ INSERT IGNORE INTO `permissions` (`name`, `description`) VALUES
 	('user-role-delete', 'user-role-delete'),
 	('user-role-index', 'user-role-index'),
 	('user-role-update', 'user-role-update'),
-	('user-update', 'user-update'),
-	('WRITE', 'write');
+	('user-update', 'user-update');
 /*!40000 ALTER TABLE `permissions` ENABLE KEYS */;
 
 -- 导出  表 shiro.roles 结构
@@ -59,7 +56,7 @@ CREATE TABLE IF NOT EXISTS `roles` (
   PRIMARY KEY (`name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
--- 正在导出表  shiro.roles 的数据：~4 rows (大约)
+-- 正在导出表  shiro.roles 的数据：~3 rows (大约)
 /*!40000 ALTER TABLE `roles` DISABLE KEYS */;
 INSERT IGNORE INTO `roles` (`name`, `description`) VALUES
 	('ADMIN', 'Administrator role'),
@@ -76,7 +73,7 @@ CREATE TABLE IF NOT EXISTS `roles_permissions` (
   KEY `RP_2` (`permission`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
--- 正在导出表  shiro.roles_permissions 的数据：~4 rows (大约)
+-- 正在导出表  shiro.roles_permissions 的数据：~23 rows (大约)
 /*!40000 ALTER TABLE `roles_permissions` DISABLE KEYS */;
 INSERT IGNORE INTO `roles_permissions` (`role_name`, `permission`) VALUES
 	('ADMIN', 'permission-create'),
@@ -102,12 +99,9 @@ INSERT IGNORE INTO `roles_permissions` (`role_name`, `permission`) VALUES
 	('USER_P1', 'permission-index'),
 	('USER_P1', 'role-index'),
 	('USER_P1', 'role-permission-index'),
-	('USER_P1', 'user-create'),
 	('USER_P1', 'user-index'),
 	('USER_P1', 'user-role-index'),
-	('USER_P1', 'user-update'),
-	('USER_P2', 'user-index'),
-	('USER_P2', 'user-update');
+	('USER_P2', 'user-index');
 /*!40000 ALTER TABLE `roles_permissions` ENABLE KEYS */;
 
 -- 导出  表 shiro.users 结构
@@ -121,7 +115,6 @@ CREATE TABLE IF NOT EXISTS `users` (
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
 INSERT IGNORE INTO `users` (`username`, `password`) VALUES
 	('admin', '$shiro1$SHA-256$500000$N96V7blDKhZUkEo7OrWBJQ==$yqeiYAelWwG7D3XnpdKyRunMfjI65C3T+9+5zEaerpw='),
-	('test', '$shiro1$SHA-256$500000$ZRBuLtdU8zzOHJvMR0VOlw==$TVcAcabDvsNwmWwZxBQy5QSZ8kh1Da1xauAyVO0c8pI='),
 	('u1', '$shiro1$SHA-256$500000$QmLtx8PaCMe72i+yVuqH+A==$P5ohK5uWi30u38ujuTnmmeUK2gPwqhxTnke2wd9fZXw='),
 	('u2', '$shiro1$SHA-256$500000$QmLtx8PaCMe72i+yVuqH+A==$P5ohK5uWi30u38ujuTnmmeUK2gPwqhxTnke2wd9fZXw=');
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
@@ -135,12 +128,10 @@ CREATE TABLE IF NOT EXISTS `users_roles` (
   KEY `UR_2` (`role_name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
--- 正在导出表  shiro.users_roles 的数据：~2 rows (大约)
+-- 正在导出表  shiro.users_roles 的数据：~3 rows (大约)
 /*!40000 ALTER TABLE `users_roles` DISABLE KEYS */;
 INSERT IGNORE INTO `users_roles` (`username`, `role_name`) VALUES
 	('admin', 'ADMIN'),
-	('admin', 'USER_P1'),
-	('test', 'ADMIN'),
 	('u1', 'USER_P1'),
 	('u2', 'USER_P2');
 /*!40000 ALTER TABLE `users_roles` ENABLE KEYS */;
